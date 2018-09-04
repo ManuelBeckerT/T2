876a6cb62e12eb2d54b857d0d98c60e086f8af3e
Manuel Becker <mabecker@uc.cl>
2018-09-04 15:13:09 -0300

    Avance lectura archivo

--- src/life/main.c
+++ src/life/main.c
@@ -4,6 +4,33 @@
 #include <unistd.h>
 #include <string.h>
 
+#define MAX_SIZE 256
+
+struct Cell
+{
+	int viva;
+	int x;
+	int y;
+};
+
+typedef struct Cell Cell;
+
+struct Tablero
+{
+	int size_x;
+	int size_y;
+
+	char * name;
+	int cell_count;
+	int position;
+
+	Cell *** cells_matrix;
+	Cell ** cells_array;
+
+};
+
+typedef struct Tablero Tablero;
+
 int count_lines(char * archivo){
 	FILE* input_file = fopen(archivo, "r");
 	int count = 0;  // Line counter (result)
@@ -24,30 +51,21 @@ int count_lines(char * archivo){
 	return count;
 }
 
+void tablero_init(Tablero * tablero, char * name){
+	tablero -> name = calloc(1, sizeof(name) + 1);
+	strcpy(tablero -> name, name);
+}
+
 int main(int argc, char** argv)
 {
-
-	int quantum = 3;
-	if (argc > 4)
-	{
-		printf("Modo de uso: ./scheduler input.txt output.csv 5\n");
+	if (argc > 3 || argc < 3){
+		printf("Modo de uso ./life <file> <t>\n");
 		return 0;
 	}
-	else if (argc < 3){
-		printf("Ingrese archivos de input.txt y output.csv para correr el programa\n");
-		return 0;
-	}
-	else if (argc == 4){
-		quantum = atoi(argv[3]);
-	}
-
-	printf("QUANTUM: %i\n", quantum);
+	int execution_time = atoi(argv[2]);
+	printf("EXECUTION TIME: %i\n", execution_time);
 
 	FILE* input_file = fopen(argv[1], "r");
-
-	int num = count_lines(argv[1]);
-	printf("NUMERO DE LINEAS %i\n\n", num);
-
 	/* Falló la apertura del archivo */
 	if(!input_file)
 	{
@@ -55,47 +73,81 @@ int main(int argc, char** argv)
 		return 2;
 	}
 
+	int table_count, a, b, c, d;
+	fscanf(input_file, "%i %i  %i %i %i", &table_count, &a, &b, &c, &d);
+
+	printf("INPUT INFO\nTable count: %i\nA: %i\nB: %i\nC: %i\nD: %i\n", table_count, a, b, c, d);
+
 	//######################################
 	//###    LECTURA DEL ARCHIVO TXT     ###
 	//######################################
 
 	// Basado en esta página https://rosettacode.org/wiki/Read_a_file_line_by_line
 	// LECTURA INPUT.TXT linea por linea
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t read;
-	if (input_file == NULL)
-		exit(EXIT_FAILURE);
 
+	Tablero ** tableros = malloc(sizeof(Tablero *)*table_count);
 	int position = 0;
-	while ((read = getline(&line, &len, input_file)) != -1){
-		Proceso * proceso = malloc(sizeof(Proceso));
-		proceso -> ID = position;
-		position++;
 
+	int input_count = 0;
+	while (table_count != input_count){
+		printf("\nNUEVO TABLERO\n");
+		Tablero * tablero = malloc(sizeof(Tablero));
+		tableros[position] = tablero;
+		printf("POSITION %i\n", position);
+		position ++;
 
-		printf("%s", line);
-		char* token = strtok(line, " ");
-		proceso_init(proceso, token);
-		token = strtok(NULL, " ");
+		//printf("%s", line);
+		char name[MAX_SIZE];
+		fscanf(input_file, "%s ", name);
+		tablero -> name = name;
+		printf("%s\n", tablero -> name);
+		int cell_count;
+		fscanf(input_file, "%i ", &cell_count);
+		tablero -> cell_count = cell_count;
+		printf("%i\n", tablero -> cell_count);
+		tablero -> cells_array = malloc(sizeof(Cell *)*cell_count);
+		tablero -> position = 0;
 
-		proceso -> tiempo_llegada = atoi(token);
-		token = strtok(NULL, " ");
-		proceso -> num_etapas = atoi(token);
-		token = strtok(NULL, " ");
-		proceso -> time_list = queue_init();
+		Cell ** cells_array = tablero -> cells_array;
+		while (cell_count > 0){
+			Cell * cell = malloc(sizeof(Cell));
+			cells_array[tablero -> position] = cell;
+			tablero -> position ++;
+			int x, y;
 
-		while (token != NULL){
-			token = strtok(NULL, " ");
+			if (cell_count == 1){
+				fscanf(input_file, "%i %i\n", &x, &y);
+				cell -> x = x;
+				cell -> y = y;
+			}
+			else{
+				fscanf(input_file, "%i %i ", &x, &y);
+				cell -> x = x;
+				cell -> y = y;
+			}
+			printf("%i %i\n", cell -> x, cell -> y);
+			cell_count --;
 		}
-
+		input_count ++;
 	}
-	free(line);
 
 
 	//######################################
 	//##   SIMULACION DE LOS PROCESOS     ##
 	//######################################
+	printf("\nLIBERANDO MEMORIA\n");
+	for (int tablero_count = 0; tablero_count < table_count; tablero_count ++){
+		Tablero * current_tablero = tableros[tablero_count];
+		printf("\nCURRENT TABLERO %s\n", current_tablero -> name);
+		Cell ** array = current_tablero -> cells_array;
+		for (int i = 0; i < current_tablero -> cell_count; i++){
+			printf("CELL POSITION %i, %i\n", array[i] -> x, array[i] -> y);
+			free(array[i]);
+		}
+		free(array);
+		free(current_tablero);
+	}
+	free(tableros);
 
 
 	fclose(input_file);
